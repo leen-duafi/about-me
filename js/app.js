45d6efd70682fc42ea4cd6543d2723172af37227
leen-duafi <leen-hala@hotmail.com>
2021-05-25 15:15:25 +0300

    ok'

--- js/app.js
+++ js/app.js
@@ -1,52 +1,91 @@
 'use strict'
 
 
-let userName=prompt('what is your name ?');
- //console.log(userName)
+/* let userName=prompt('what is your name ?');
+//console.log(userName)
+
+while(!userName){userName=prompt('what is your name ?')}
  
 
 alert('welcome to my page ' +  userName   + ' this page was made so you can learn more about me through a gussing game i will ask you a couble of question and you can answer with yes or no. for every correct answer you will got a point. okay!!lets go ')
 
+let score=0
 
 let book=prompt('harry potter is my favorite book?');
- //conole.lo(book)
- book=book.toLowerCase();
+//conole.lo(book)
+book=book.toLowerCase();
 
- if(book=="yes"){alert("no harry potter is a good book but my favorite is six of crows")}
- else{alert('one point for you :  harry potter is not my favorite book . my favorit book is six of crows')}
+while(!book){book=prompt('harry potter is my favorite book?')}
+
+if(book=="yes" || book=="y" ){alert("no harry potter is a good book but my favorite is six of crows")
+}
+else if (book=="no" || book=="n"){ alert('one point for you :  harry potter is not my favorite book . my favorit book is six of crows');score++}
+else (alert("please answer yes/y or no/n "))
 
 let drink=prompt('my favorite drink is hot choclate?');
- // console.log(drink)
- drink=drink.toLowerCase();
+// console.log(drink)
+drink=drink.toLowerCase();
 
- if(drink=="yes"){alert(" that is right. one pont for you")}
-  else{alert("my favorit drink IS HOT CHOCLATE ")}
+while(!drink){drink=prompt('my favorite drink is hot choclate?')}
+
+if(drink=="yes"|| drink =="y"){alert(" that is right. one pont for you");
+score++}
+else if (drink=="no" || drink=="n"){ alert(' my favorit drink IS HOT CHOCLATE ')}
+ else(alert("please answer yes/y or no/n "))
 
 let movie=prompt('my favorite movie is home alone ?');
- // console.log(movie)
- movie=movie.toLowerCase();
-
- if (movie=="yes") {alert("no that is not corect my fovirt movie is READY OR NOT ")   
- }else{alert("that is right you got a point my fovirt movie is READY OR NOT ")}
+// console.log(movie)
+movie=movie.toLowerCase();
+while(!movie){movie=prompt('my favorite movie is home alone ?')}
 
 
-let food=prompt('i prefer home made food rather than restraunt food?');
-  // console.log(food)
-  food=food.toLowerCase();
-  if (food=="yes") {alert('that is right mother food is the best. one pont for you ' )
-    
-}else{alert('i do love fast food but my mother food is the best ')}
+if (movie=="yes" || movie=="y") {alert("no that is not corect my fovirt movie is READY OR NOT ");}
+else if (movie =="no" || movie =="n"){ alert('that is right you got a point my fovirt movie is READY OR NOT ');score++}
+else(alert("please answer yes/y or no/n"))
+ 
+ 
 
+let food=prompt('i prefer home made food rather than restraunt food?')
+ // console.log(food)
+ food=food.toLowerCase();
+ while(!food){food=prompt('i prefer home made food rather than restraunt food?')}
+
+
+ if (food=="yes" || food=="y") {alert('that is right mother food is the best. one pont for you ' );
+ score++
+   
+}else if (food=="no" || food == "n" ){alert('i do love fast food but my mother food is the best ')}
+else (alert("please answer yes/y or no/n "))
 
 let sport=prompt('i like to work out every day ?');  
-   // console.log(sport)
-   sport=sport.toLowerCase();
+  // console.log(sport)
+  sport=sport.toLowerCase();
+  while(!sport){sport=prompt('i like to work out every day ?')}
 
-if (sport=="yes") {alert('if watching tv consider working out ')
-    
-} else { alert('that is right and another point for you ')
-    
+ if (sport =="yes " || sport =="y")  {alert("if WATCHING tv consider working out ")}
+else if (sport == "no" || sport == "n"){alert('that is right and another point for you ');score++}
+else (alert('please answer yes/y or no/n "'))
+
+alert("thank you for visting our page and playing along " + userName + " your score is  " + score + " out of 7" );
+
+*/
+
+let guess = prompt("guess a number between 0-5")
+guess=Number(guess);
+while (!guess) { guess = prompt("guess a number between 0-5") }
+console.log(typeof(guess))
+for (let i = 0; i < 4 ; i++) {
+  console.log(i)
+
+  if (guess > '3') {
+    alert(" it is too high ");
+    guess = prompt("try again")
+  }
+  else if (guess < '3') {
+    alert("it is too low ");
+    guess = prompt("try again")
+  }
+  else if (guess == '3' ){alert('that is right ');break}
 }
 
-alert("thank you for visting our page and playing along " + userName );
-
+alert('the right answer is 3')
